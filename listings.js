dc067e33ff9ec4be5e14d840212a3de97985514d
Elliot Jones <119816371+elliotjonesdev@users.noreply.github.com>
2026-08-29 16:31:15 +0100

    Delete listings.js

--- listings.js
+++
@@ -1,8 +0,0 @@
-const listings = [
-  {id:1,title:"Solid Oak Bed Frame",category:"Beds",style:"Vintage",material:"Solid Oak",distance:2.4,added:"18 min ago",mins:18,pick:true,url:"https://www.gumtree.com/"},
-  {id:2,title:"Mid-Century Sideboard",category:"Storage",style:"Mid-Century",material:"Teak",distance:3.8,added:"1 hr ago",mins:60,pick:true,url:"https://www.facebook.com/marketplace/"},
-  {id:3,title:"Pine Chest of Drawers",category:"Storage",style:"Rustic",material:"Pine",distance:4.1,added:"2 hrs ago",mins:120,pick:true,url:"https://www.freecycle.org/"},
-  {id:4,title:"Wooden Dining Table",category:"Tables",style:"Modern",material:"Wood",distance:2.7,added:"3 hrs ago",mins:180,pick:true,url:"https://www.gumtree.com/"},
-  {id:5,title:"Painted Pine Bedside Tables",category:"Storage",style:"Country",material:"Pine",distance:1.6,added:"4 hrs ago",mins:240,pick:false,url:"https://www.facebook.com/marketplace/"},
-  {id:6,title:"Vintage Metal Bed Frame",category:"Beds",style:"Industrial",material:"Metal",distance:4.7,added:"5 hrs ago",mins:300,pick:false,url:"https://www.gumtree.com/"}
-];
